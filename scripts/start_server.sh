fc2ea885952780e3cf27b5dcc26f34cb36160d12
Nikhilgujela <139757274+Nikhilgujela@users.noreply.github.com>
2025-07-03 17:39:10 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-# Add PM2 path if needed (optional)
-export PATH=$PATH:/usr/local/bin
-
-# Start your Node.js app with PM2
-pm2 start /var/nodeapp/src/index.js --name nodeapp
+forever start /var/nodeapp/src/index.js
