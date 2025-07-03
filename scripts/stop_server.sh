c64077691be61c7b4e9fd146e9796f517653a03d
Nikhilgujela <139757274+Nikhilgujela@users.noreply.github.com>
2025-07-03 17:38:44 +0530

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-
-# Add PM2 path if needed (optional ) 
-export PATH=$PATH:/usr/local/bin
-
-# Stop your app
-pm2 stop nodeapp || true
-pm2 delete nodeapp || true
+cd /var/nodeapp
+forever stopall
